0423de9f80dc1c8e46fb74f4aa73f0edf97e4dba
Rajat Pandit <razatp2203@gmail.com>
2025-01-21 15:45:13 +0545

    Updated Signup frontend

--- fureverfriends/src/App.js
+++ fureverfriends/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Login  from './Components/LoginSignup/Login';
+import Signup from './Components/LoginSignup/Signup';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
         </Routes>
       </Router>
     </div>
